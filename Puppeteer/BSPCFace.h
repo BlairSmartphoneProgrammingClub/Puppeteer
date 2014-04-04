734069390fcc9875b880a27c81cd56bb1769f5c6
Chazan <363588@757-318-6-036.local>
2014-04-04 17:14:20 -0400

    This is a basic implementation that works.
    ‘murica

---
+++ Puppeteer/BSPCFace.h
@@ -0,0 +1,18 @@
+//
+//  BSPCFace.h
+//  Puppeteer
+//
+//  Created by Chazan, Jonah on 4/4/14.
+//  Copyright (c) 2014 BlairSmartphoneProgrammingClub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSPCFace : NSObject {
+    UIImage *smallPic,*largePic;
+    NSString *name;
+    
+    NSMutableArray *quotes;
+}
+
+@end
---
+++ Puppeteer/BSPCFace.m
@@ -0,0 +1,25 @@
+//
+//  BSPCFace.m
+//  Puppeteer
+//
+//  Created by Chazan, Jonah on 4/4/14.
+//  Copyright (c) 2014 BlairSmartphoneProgrammingClub. All rights reserved.
+//
+
+#import "BSPCFace.h"
+
+@implementation BSPCFace
+
+- (void) initWithName:(NSString*)name SmallImage:(UIImage*)smallImg LargeImage:(UIImage*)largeImage {
+    
+}
+
+- (void) setQuotes:(NSMutableArray*)q {
+    
+}
+
+- (void) playQuote:(NSString*)name {
+    
+}
+
+@end
